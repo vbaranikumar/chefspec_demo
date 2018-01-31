dc1cbf357864de593a81e5d41402f424b9e09c66
Barani Kumar <vbaranikumar@gmail.com>
2018-01-31 09:21:46 +0530

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chefspec_demo
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chefspec_demo
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
